4a3adce17c445386240283eaa5671d66d5736ded
DraganaVukasinovic <vukasinovic.dragana@yahoo.com>
2023-02-09 17:21:19 +0100

    omoguceno sortiranje koriscenjem korisnicki definisanih kuka

--- src/Pocetna.jsx
+++ src/Pocetna.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import Kartica from "./Kartica";
 
  
  
 
  function Pocetna({predstave,rezervisi}) {
+    const [sort, setSort] = useState(true);
+    function sortiraj(){
+    
+        setSort(!sort);
+      }
+ 
+
     return (
-       
+  
          <div className="pocetna">
-                {predstave.map((p)=><Kartica key={p.id} p={p} rezervisi={rezervisi}></Kartica>)}
-      
+      <button className="btn primary" onClick={sortiraj}>Sortiraj </button>
+            {sort === true ? 
+                    
+                    predstave
+                    .sort((a, b) => a.cena < b.cena ? -1 : 1)
+                    .map((p)=><Kartica key={p.id} p={p} rezervisi={rezervisi}></Kartica>)
+            :
+                    predstave
+                    .sort((a, b) => a.cena > b.cena ? -1 : 1)
+                    .map((p)=><Kartica key={p.id} p={p} rezervisi={rezervisi}></Kartica>)
+            }
+
+
+
+
+
+
+             
          </div>
        
     );
