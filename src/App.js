5568e333384d4535ba4ed2dfcc9726b4814dde3c
DraganaVukasinovic <vukasinovic.dragana@yahoo.com>
2023-02-09 16:39:09 +0100

    kreirana reusable komponenta futer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import Navbar from './Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Footer';
 function App() {
+
+
+
+
+
   return (
     <div className="App">
     <BrowserRouter>
@@ -15,7 +21,7 @@ function App() {
 
           </Routes>
         
-          
+          <Footer></Footer>
 
           </BrowserRouter>
     </div>
---
+++ src/Footer.jsx
@@ -0,0 +1,26 @@
+ 
+ 
+
+ function Footer() {
+   return (
+      
+        <div className="footer-content">
+            <h3>Pozoriste pinokio</h3>
+            <p>Dobrodosli u pozoriste lutaka Piokio</p>
+ 
+            <div className="footer-menu">
+            <ul className="f-menu">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Blog</a></li>
+            </ul>
+        </div>
+        <p>copyright &copy;2023 <a href="">pozoristepinokio</a>  </p>
+        </div>
+      
+   );
+ }
+ 
+ export default Footer;
+ 
