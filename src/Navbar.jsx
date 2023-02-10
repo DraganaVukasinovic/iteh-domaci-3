d106145ca07695d5d9a1a2f354de2be2a4bcda74
DraganaVukasinovic <vukasinovic.dragana@yahoo.com>
2023-02-10 16:18:09 +0100

    finalne izmene

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,11 +4,11 @@
 function Navbar() {
   return (
      
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         
         < Link  to="/" className="navbar-brand">Pocetna</Link>
 
-        < Link  to="/rezervacije" className="nav-link">Moje rezervacije</Link>
+        < Link  to="/rezervacije" className="navbar-brand">Moje rezervacije</Link>
  
 
 
